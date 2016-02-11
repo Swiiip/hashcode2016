3179ef89c97a8940c1fd25979de6667e65157700
antoine-cha <antoine.chassang0@gmail.com>
2016-02-11 20:14:01 +0100

    add print method

--- simulation.py
+++ simulation.py
@@ -1,18 +1,27 @@
-class Simulation:
+import parser
 
 
-    def __init__(self, paramDict, commandList):
+class Simulation(object):
+
+    def __init__(self, paramDict):
         for e in paramDict:
             setattr(self, e, paramDict[e])
 
+    def __str__(self):
+        s = ''
+        for e in self.__dict__:
+            s += e + '\n'
+            s += str(self.__dict__[e]) + '\n'
+        return s
+
     def step(self):
         print "caca"
-        
-        
+
+
 if __name__ == "__main__":
-
-    sim = Simulation({"test" : "test2"})
-
-    for i in range(sim.nbrRounds):
+    input_file = 'data/busy_day.in'
+    params = parser.parse(input_file)
+    sim = Simulation(params)
+    print(sim)
+    for i in range(sim.turns):
         sim.step();
-
