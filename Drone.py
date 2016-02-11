9c14c90e0c9d92faadaaf405d4651871d6611af6
Clement Pinard <clement.pinard@parrot.com>
2016-02-11 21:38:51 +0100

    penis

--- Drone.py
+++ Drone.py
@@ -5,14 +5,13 @@ Created on 11 fevr. 2016
 '''
 import Warehouse
 import numpy as np
-from IPython.config.application import catch_config_error
 class Drone(object):
     '''
     classdocs
     '''
 
 
-    def __init__(self, location, payloadWeight):
+    def __init__(self, location, payloadWeight, weights):
         '''
         Constructor
         '''
@@ -20,6 +19,11 @@ class Drone(object):
         self.payload = {}
         self.payloadWeight = payloadWeight
         self.currentPayloadWeight = 0
+        self.busy = False
+        self.commandType = 'W'
+        self.command = []
+        self.weights = weights
+        self.timeleft = 0
         
     def goto(self, location):
         distance = np.sqrt((self.pos[0]-location[0])*(self.pos[0]-location[0]) + (self.pos[1]-location[1])*(self.pos[1]-location[1]))
@@ -29,7 +33,8 @@ class Drone(object):
     def gotoWH(self,warehouse):
         return self.goto(warehouse.location)
     
-    def load(self,objet,n,weight,warehouse):
+    def load(self,objet,n,warehouse):
+        weight = self.weights[objet]
         if weight*n + self.currentPayloadWeight <= self.payloadWeight:
             self.currentPayloadWeight += weight*n
             if objet in self.payload :
@@ -42,8 +47,41 @@ class Drone(object):
         
         return 1
     
-    def unload(self, objet,weight,n, warehouse):
+    def takeCommand(self,commandType,param):    
+        self.command = param
+        self.commandType = commandType
+        if commandType == 'D':
+            self.timeleft = self.goto(param[0]) + 1
+            self.busy = True
+        if commandType == 'W':
+            self.timeleft = param[0]
+            self.busy = True 
+        if commandType == 'L' or commandType == 'U':
+            self.timeleft = self.gotoWH(param[0])
+            self.busy =True
+        
+        return 0
+    
+    def step(self):
+        if self.timeleft>1:
+            self.timeleft -=1
+        else:
+            self.timeleft = 0
+            self.execute()
+            
+            
+    def execute(self):
+        if self.commandType == 'D':
+            self.deliver(self.command[2],self.command[3])
+        if self.commandType == 'L':
+            self.load(self.command[2],self.command[3],self.command[1])
+        if self.commandType == 'U':
+            self.unload(self.command[2],self.command[3],self.command[1])
+        self.busy =True
+    
+    def unload(self, objet,n, warehouse):
         unloaded = 0
+        weight = self.weights[objet]
         if objet not in self.payload:
             return unloaded
         if self.payload[objet] >= n:
@@ -58,7 +96,9 @@ class Drone(object):
             self.payload[objet] = 0
         return unloaded
     
-    def deliver(self,objet,weight,n):
+    
+    def deliver(self,objet,n):
+        weight = self.weights[objet]
         delivered = 0
         if objet not in self.payload:
             return delivered
@@ -79,13 +119,20 @@ class Drone(object):
         + "current weight " + str(self.currentPayloadWeight)
         
 if __name__ == "__main__":
-    D = Drone((10,10),30)
+    D = Drone((10,10),30,[1,1,1,1])
     W = Warehouse.Warehouse((2,5), {0:5,2:6,4:7})
     print D
     print D.goto((24,34))
     print D.gotoWH(W)
-    print D.load(0, 4, 1, W)
-    print D.unload(0, 1, 2, W)
-    print D.deliver(0,1,1)
+    print D.load(0, 4, W)
+    print D.unload(0, 1, W)
+    print D.deliver(0,1)
+    D.takeCommand('W', [4])
+    print D.busy
+    D.step()
+    D.step()
+    D.step()
+    D.step()
+    print D.busy
     print "BONGEOURRE"
         
